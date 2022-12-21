7378e8616ee33443aa072720f6d980a6d3a81fdd
Muhammad-Abid11 <abidkhan11845@gmail.com>
2022-12-22 02:53:28 +0500

    Exit loop

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+
+
 // FIRST-CLASS FUNCTIONS
+
+/*
 
 const greeting = () => `Hello`
 console.log("function body", greeting)
@@ -17,3 +21,36 @@ const sayHello = function () {
 console.log("1 function without paranthesis", sayHello)
 console.log("2 function with paranthesis", sayHello())
 console.log("3 function with double paranthesis", sayHello()())
+
+*/
+
+// Exit Loop
+
+// by -->break
+
+let a = [1, 2, 3, 4, 5, 6]
+for (const value of a) {
+    console.log("values stop by Break -->", value);
+    if (value == 3) {
+        break;
+    }
+}
+
+// by-->.some
+
+let arr = [1, 2, 3, 4, 5, 6];
+arr.some(number => {
+    console.log("some-->", number);
+    return number > 2
+})
+// koye statement true hoye too loop exit
+
+
+
+// by-->.every
+
+let arr3 = [1, 2, 3, 4, 5, 6];
+arr.every(number => {
+    console.log("every-->", number);
+    return number < 3
+})
