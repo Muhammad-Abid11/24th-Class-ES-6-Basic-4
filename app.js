11eacd4d0c109baa8201630dd66de73cc10b180a
Muhammad-Abid11 <abidkhan11845@gmail.com>
2023-01-29 04:03:24 +0500

    Copy by Value And Copy by Reference

--- app.js
+++ app.js
@@ -54,3 +54,46 @@ arr.every(number => {
     console.log("every-->", number);
     return number < 3
 })
+
+
+
+/*
+
+what is difference between "copy by Value" and "Copy by Reference"
+
+In "copy by value," a new memory location is created and the value of the 
+original variable is copied to the new location. The two variables then have
+separate memory locations and any changes made to one will not affect the
+other. In "copy by reference," a reference to the memory location of the
+original variable is copied, so the two variables refer to the same
+memory location. Any changes made to one will affect the other.
+
+Example
+
+// Copy by value
+let x = 5;
+let y = x;
+x += 1;
+console.log(x);  // 6
+console.log(y);  // 5
+
+// Copy by reference
+let a = { value: 5 };
+let b = a;
+a.value += 1;
+console.log(a.value);  // 6
+console.log(b.value);  // 6
+
+In the first example, x and y are primitive data types, and when 
+we assign x to y, it creates a new memory location for y and copies 
+the value of x to y. So changing value of x does not affect the value of y.
+
+In the second example, a and b are objects, and when we assign a to b,
+it assigns the reference of a to b. So both a and b are pointing to 
+the same memory location. Changing the value of a will affect the value
+of b as they are pointing to the same memory location
+
+
+
+
+*/
